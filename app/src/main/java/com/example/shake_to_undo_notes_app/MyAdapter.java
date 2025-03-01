5f0eafbfb8473b3839ccea097afa3ecdc3e175a0
R-I-T-U <ritukhwalapala@gmail.com>
2025-03-01 22:26:03 +0545

    Delete note by long press

--- app/src/main/java/com/example/shake_to_undo_notes_app/MyAdapter.java
+++ app/src/main/java/com/example/shake_to_undo_notes_app/MyAdapter.java
@@ -1,10 +1,14 @@
 package com.example.shake_to_undo_notes_app;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,11 +34,33 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
         Note note = notes.get(position);
         holder.title.setText(note.getTitle());
         holder.desc.setText(note.getDesc());
         holder.time.setText(new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault()).format(note.getTime()));
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                PopupMenu menu = new PopupMenu(context, v);
+                menu.getMenu().add("DELETE");
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if(item.getTitle().equals("DELETE")){
+                           notes.remove(position);
+                           notifyItemRemoved(position);
+                           notifyItemRangeChanged(position, notes.size()-position);
+                            Toast.makeText(context, "Note deleted", Toast.LENGTH_SHORT).show();
+                        }
+                        return true;
+                    }
+                });
+                menu.show();
+                return true;
+            }
+        });
     }
 
     @Override
@@ -43,8 +69,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     public void addNote(Note note) {
-        notes.add(note);
-        notifyItemInserted(notes.size() - 1);
+        notes.add(0, note);
+        notifyItemInserted(0);
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
